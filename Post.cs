c0f7649443057f1df5601a45ead5a4511efa06a3
Frank <fboucher@outlook.com>
2023-03-12 15:50:21 -0400

    adding class Post

---
+++ Post.cs
@@ -0,0 +1,22 @@
+
+    public class Post
+    {
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public string? Date_published { get; set; }
+        public string? Dek { get; set; }
+        public string? Lead_image_url { get; set; }
+        public string? Next_page_url { get; set; }
+        public string? Url { get; set; }
+        public string? Domain { get; set; }
+        public string? Excerpt { get; set; }
+        public int Word_count { get; set; }
+        public string? Direction { get; set; }
+        public int Total_pages { get; set; }
+        public int Rendered_pages { get; set; }
+        public string Id { get; set; }
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+    }
+
+
