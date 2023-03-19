d6fc842c450f82649725fde6b7823d874c067271
Frank <fboucher@outlook.com>
2023-03-19 17:56:26 -0400

    set post class as Entity table

--- Post.cs
+++ Post.cs
@@ -1,23 +1,26 @@
 
-   /// <summary>Data saved to Azure Table Storage from a post in the pre-read process</summary>
-    public class Post
-    {
-        public string? Title { get; set; }
-        public string? Author { get; set; }
-        public string? Date_published { get; set; }
-        public string? Dek { get; set; }
-        public string? Lead_image_url { get; set; }
-        public string? Next_page_url { get; set; }
-        public string? Url { get; set; }
-        public string? Domain { get; set; }
-        public string? Excerpt { get; set; }
-        public int Word_count { get; set; }
-        public string? Direction { get; set; }
-        public int Total_pages { get; set; }
-        public int Rendered_pages { get; set; }
-        public string Id { get; set; }
-        public string PartitionKey { get; set; }
-        public string RowKey { get; set; }
-    }
+using Azure.Data.Tables;
+/// <summary>Data saved to Azure Table Storage from a post in the pre-read process</summary>
+public class Post : ITableEntity
+{
+    public string? Title { get; set; }
+    public string? Author { get; set; }
+    public string? Date_published { get; set; }
+    public string? Dek { get; set; }
+    public string? Lead_image_url { get; set; }
+    public string? Next_page_url { get; set; }
+    public string? Url { get; set; }
+    public string? Domain { get; set; }
+    public string? Excerpt { get; set; }
+    public int Word_count { get; set; }
+    public string? Direction { get; set; }
+    public int Total_pages { get; set; }
+    public int Rendered_pages { get; set; }
+    public string Id { get; set; }
+    public string PartitionKey { get; set; }
+    public string RowKey { get; set; }
+    public DateTimeOffset? Timestamp { get; set; }
+    public Azure.ETag ETag { get; set; }
+}
 
 
