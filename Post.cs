ea546f529ad41f54c9b853856e47dec38fcabc4a
Frank <fboucher@outlook.com>
2023-03-20 07:38:21 -0400

    mapping to lower case properties

--- Post.cs
+++ Post.cs
@@ -1,25 +1,57 @@
 
+using System.Runtime.Serialization;
 using Azure.Data.Tables;
 /// <summary>Data saved to Azure Table Storage from a post in the pre-read process</summary>
 public class Post : ITableEntity
 {
+    [DataMember(Name="title")]
     public string? Title { get; set; }
+    
+    [DataMember(Name="author")]
     public string? Author { get; set; }
+
+    [DataMember(Name="date_published")]
     public string? Date_published { get; set; }
+
+    [DataMember(Name="dek")]
     public string? Dek { get; set; }
+
+    [DataMember(Name="lead_image_url")]
     public string? Lead_image_url { get; set; }
+
+    [DataMember(Name="next_page_url")]
     public string? Next_page_url { get; set; }
+
+    [DataMember(Name="url")]
     public string? Url { get; set; }
+
+    [DataMember(Name="domain")]
     public string? Domain { get; set; }
+
+    [DataMember(Name="excerpt")]
     public string? Excerpt { get; set; }
+
+    [DataMember(Name="word_count")]
     public int Word_count { get; set; }
-    public string? Direction { get; set; }
+
+   [DataMember(Name="direction")]
+     public string? Direction { get; set; }
+
+    [DataMember(Name="total_pages")]
     public int Total_pages { get; set; }
+
+    [DataMember(Name="rendered_pages")]
     public int Rendered_pages { get; set; }
+
+   [DataMember(Name="id")]
     public string Id { get; set; }
+
     public string PartitionKey { get; set; }
+
     public string RowKey { get; set; }
+
     public DateTimeOffset? Timestamp { get; set; }
+
     public Azure.ETag ETag { get; set; }
 }
 
