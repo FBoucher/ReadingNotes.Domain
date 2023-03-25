92bf3f7f3e3a56c67aa4512d6aea0a45420f934d
Frank <fboucher@outlook.com>
2023-03-25 07:38:02 -0400

    switch to dictionary to have type

--- Notes.cs
+++ Notes.cs
@@ -8,12 +8,12 @@ public class Notes
     {
         this.Number = number;
         this.Title = $"Reading Notes #{number}";
-        this.NoteGroups = new Hashtable();
+        this.NoteGroups = new Dictionary<string, List<Note>>();
     }
     public int Number { get; set; }
     public string  Title { get; set; } = string.Empty;
     public string  Filename { get; set; } = string.Empty;
     public string Tags { get; set; } = string.Empty;
     public string Intro { get; set; } = string.Empty;
-    public Hashtable NoteGroups { get; set; }
+    public Dictionary<string, List<Note>> NoteGroups { get; set; }
 }
