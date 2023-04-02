b694ffd64234ab1aa7203b63837ea7216a70c11a
Frank <fboucher@outlook.com>
2023-04-02 17:07:21 -0400

    version 1.1.8beta

---
+++ Summary.cs
@@ -0,0 +1,22 @@
+
+using System.Runtime.Serialization;
+using Azure.Data.Tables;
+public class Summary : ITableEntity
+{
+    public string PartitionKey { get; set; }
+    public string RowKey { get; set; }
+    public Azure.ETag ETag { get; set; }
+    public DateTimeOffset? Timestamp { get; set; }
+
+    [DataMember(Name = "id")]
+    public string Id { get; set; }
+    
+    [DataMember(Name = "title")]
+    public string? Title { get; set; }
+
+  [DataMember(Name = "filename")]
+  public string? FileName { get; set; }
+
+    [DataMember(Name = "isgenerated")]
+  public string? IsGenerated { get; set; }
+}
